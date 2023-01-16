383a1c3ee28ef96b7870b050c7c8df139febf10f
Apriliani210401 <nurindah01apriliani@gmail.com>
2023-01-16 09:42:50 +0800

    indah cantik

---
+++ home.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 text-center">
+            <p>&nbsp;</p>
+            <h4>NIM : 20202205081</h4>
+            <h4>Nama Mahasiswa : Nurlinda Apriliani</h4>
+            </div>
+    </div>
+</div>
